c6fd04af3a6235767306b975e72841a48a1d2964
SHANTO <rabbymd55555@gmail.com>
2025-02-17 23:00:24 +0600

    task-1.js

---
+++ task-1.js
@@ -0,0 +1,14 @@
+function calculateVAT(price) {
+  if (typeof price !== "number" || price <= 0) {
+    return "Invalid";
+  }
+  const vat = (price * 7.5) / 100;
+  return vat;
+}
+
+console.log(calculateVAT(1500));
+console.log(calculateVAT(200));
+console.log(calculateVAT(999));
+console.log(calculateVAT(-500));
+console.log(calculateVAT("101"));
+console.log(calculateVAT("foo"));
