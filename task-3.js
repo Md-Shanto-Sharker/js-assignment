5e4b6239060df73d3974e016f422cbabf91e4a8d
SHANTO <rabbymd55555@gmail.com>
2025-02-17 23:26:15 +0600

    task-3.js

---
+++ task-3.js
@@ -0,0 +1,23 @@
+function willSuccess(marks) {
+  if (!Array.isArray(marks)) {
+    return "Invalid";
+  }
+  let passCount = 0;
+  let failCount = 0;
+  for (let mark of marks) {
+    if (mark >= 50) {
+      passCount++;
+    } else {
+      failCount++;
+    }
+  }
+  return passCount > failCount;
+}
+console.log(willSuccess([60, 70, 80, 40, 30]));
+console.log(willSuccess([48, 48, 92, 100]));
+console.log(willSuccess([48, 48, 50, 50, 100]));
+console.log(willSuccess([]));
+console.log(willSuccess([90]));
+console.log(willSuccess([90, 99, 87, 48, 34, 49]));
+console.log(willSuccess("100 , 100"));
+console.log(willSuccess(90));
